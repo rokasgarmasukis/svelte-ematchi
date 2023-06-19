93e7ef2ee8b4748b449bdd812134107a5b40dbff
Rokas Garmasukis <rokas.garmasukis@gmail.com>
2023-06-19 14:00:54 +0200

    Set up emoji grid

--- src/routes/Game.svelte
+++ src/routes/Game.svelte
@@ -1,34 +1,58 @@
+<script lang="ts">
+  import Grid from './Grid.svelte';
+  import { levels } from './levels';
+  import type { Level } from './levels';
+
+  const level = levels[0];
+  let size: number = level.size;
+  let grid: string[] = create_grid(level);
+  let found: string[] = [];
+
+  function create_grid(level: Level): string[] {
+    const copy = level.emojis.slice();
+    const pairs: string[] = [];
+
+    for (let i = 0; i < (level.size * level.size) / 2; i++) {
+      const index = Math.floor(Math.random() * copy.length);
+      const emoji = copy[index];
+
+      copy.splice(index, 1);
+      pairs.push(emoji);
+    }
+    pairs.push(...pairs);
+
+    return pairs;
+  }
+</script>
+
 <div class="game">
-    <div class="info">
+  <div class="info" />
+  <div class="grid-container">
+    <Grid {grid}/>
+  </div>
 
-    </div>
-    <div class="grid-container">
-
-    </div>
-
-    <div class="info">
-
-    </div>
+  <div class="info" />
 </div>
 
 <style>
-    .game {
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-        height: 100%;
-    }
+  .game {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+    font-size: min(1vmin, 0.5rem);
+  }
 
-    .info {
-        width: 80vmin;
-        height: 10vmin;
-        background: purple;
-    }
+  .info {
+    width: 80em;
+    height: 10em;
+    background: purple;
+  }
 
-    .grid-container {
-        width: 80vmin;
-        height: 80vmin;
-        background: teal;
-    }
+  .grid-container {
+    width: 80em;
+    height: 80em;
+    background: teal;
+  }
 </style>
---
+++ src/routes/Square.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  export let emoji: string;
+</script>
+
+<div class="square">
+  <span>{emoji}</span>
+</div>
+
+<style>
+  .square {
+    background: yellow;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  span {
+    font-size: 5em;
+  }
+</style>
